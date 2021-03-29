daa0bb325c164c3f6254dbc2cbc7e3bb06cf527e
Arecio Canton <cuba289@gmail.com>
2021-03-29 00:52:07 -0400

    added first set of questions put time and highscores in correct place and did transition

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        title: "How many days are in a year?",
+        choice: ["365", "300", "200", "364"],
+        answer: "365"
+    },
+    {
+        title: "How many planets are in the Solar System?",
+        choice: ["8", "7", "9", "10"],
+        answer: "8"
+    },
+    {
+        title: "What is the planet closest to the Sun?",
+        choice: ["Venus", "Saturn", "Mars", "Mercury"],
+        answer: "Mercury"
+    },
+    {
+        title: "What is the planet farthest from the Sun?",
+        choice: ["Saturn", "Neptune", "Uranus", "Pluto"],
+        answer: ["Neptune"]
+    },
+    {
+        title: "What is the biggest planet in our Solar System?",
+        choice: ["Saturn", "Jupiter", "Earth", "Uranus"]
+    }
+]
+
+var answers = ["365", "8", "Mercury"]
+
